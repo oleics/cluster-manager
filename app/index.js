6d8923e01b5ea9a82dea6c867748b5566c536d03
Oliver Leics <oliver.leics@gmail.com>
2011-12-14 23:56:20 +0100

    Two more play-lines in the default app.

--- app/index.js
+++ app/index.js
@@ -1,5 +1,7 @@
 
 var main = exports.main = function(options, cb) {
     setInterval(function(){console.log(process.pid, (+(new Date())));}, 2000);
+    //setTimeout(function(){throw new Error('Test Error');}, 7000);
+    //setTimeout(function(){process.exit(0);}, 10000);
     cb();
 };
